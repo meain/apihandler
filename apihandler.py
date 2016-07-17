f051478012c851ff68fc52d034aec0344ba6edc8
Abin Simon <abinsimon10@gmail.com>
2016-07-17 22:47:44 +0530

    Add validity check for api data

--- apihandler.py
+++ apihandler.py
@@ -20,6 +20,20 @@ class apihandler:
     api_cache = db.collection('apicache')
     api_cache.create()
 
+    @property
+    def cache_validity(self):
+        '''
+        Default = '' = unlimited
+        You could use s(second) m(minute) h(hour) d(day) m(month) y(year)
+        *only one at a time*
+        eg :
+            5s
+            30d
+            10m
+            1y
+        '''
+        return ''
+
     def write_cache(self, data, mode, result):
         '''
         Write cache to db
@@ -31,7 +45,7 @@ class apihandler:
         pickled_data = pickle.dumps(data)
         pickled_mode = pickle.dumps(mode)
         pickled_result = pickle.dumps(result)
-        pickled_time = pickle.dumps(datetime.datetime.now())
+        pickled_time = pickle.dumps(datetime.datetime.now().replace(microsecond=0))
         self.api_cache.store({'data': pickled_data, 'mode': pickled_mode, 'result': pickled_result, 'class_name': class_name, 'time': pickled_time})
 
     def read_cache(self, data, mode):
@@ -43,11 +57,42 @@ class apihandler:
         class_name = self.__class__.__name__
 
         cache_data = self.api_cache.filter(lambda api: api['data']==pickle.dumps(data) and api['mode']==pickle.dumps(mode) and api['class_name']==class_name)
+        if len(cache_data) > 0 :
+            cache_result = cache_data[-1]
+            # Check validity of cache
+            validity = self.cache_validity
+            save_time = pickle.loads(cache_result['time'])
+            current_time = datetime.datetime.now().replace(microsecond=0)
+            time_difference = (current_time - save_time).total_seconds()
+            # Get validity
+            if len(validity) > 0 :
+                if validity[-1] == 's':
+                    valid_seconds = int(validity[:-1])
+                elif validity[-1] == 'm':
+                    valid_seconds = int(validity[:-1])*60
+                elif validity[-1] == 'h':
+                    valid_seconds = int(validity[:-1])*60*60
+                elif validity[-1] == 'd':
+                    valid_seconds = int(validity[:-1])*24*60*60
+                elif validity[-1] == 'm':
+                    valid_seconds = int(validity[:-1])*30*24*60*60
+                elif validity[-1] == 'y':
+                    valid_seconds = int(validity[:-1])*12*30*24*60*60
+            else:
+                # Infinite validity
+                valid_seconds = 0
+                time_difference = 0
+            # Remove if api data not valid anymore
+            if valid_seconds < time_difference :
+                cache_result = None
+        else :
+            cache_result = None
+
         # cached_data[-1] jsut because it might have old data, this will ensure it takes the last one
-        if not cache_data or cache_data[-1]['result'] is None:
+        if not cache_result or cache_result['result'] is None:
             return None
         else :
-            return pickle.loads(cache_data[-1]['result'])
+            return pickle.loads(cache_result['result'])
 
     def call(self, data, mode):
         '''
@@ -74,6 +119,5 @@ class apihandler:
             return result
         else:
             # Cached data available
-            # TODO : Check time so as to limit use of old cache data
             result = cached_data
             return result
--- wolfram.py
+++ wolfram.py
@@ -7,6 +7,10 @@ class apiwolfram(apihandler):
     def __init__(self):
         self.client = wolframalpha.Client(key_wolfram.key)
 
+    @property
+    def cache_validity(self):
+        return '5s'
+
     def call(self, data, mode):
         '''
         Data : string - 'wolfram query'
